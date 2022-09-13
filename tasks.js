bbb12ddd05708beab90a0b94bed34afa776344c0
jugosoft <46106133+jugosoft@users.noreply.github.com>
2022-09-13 17:40:41 +0400

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -9,7 +9,7 @@ const calculate_drunk_turtle = (n, day=0) => {
 
 console.log(`Turtle climbs for ${calculate_drunk_turtle(0)} days.`); // 4
 
-const calculate_handshakes = men => (men - 1) * men / 2;
+const calculate_handshakes = men_count => (men_count - 1) * men_count / 2;
 console.log(`Men has performed ${calculate_handshakes(10)} handschakes total.`); // 45
 
 const remove_dublicates = (input_words) => {
