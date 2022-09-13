5b107c42809d80212ee031020f42602252b2dc19
jugosoft <46106133+jugosoft@users.noreply.github.com>
2022-09-13 17:35:39 +0400

    Create tasks.js

---
+++ tasks.js
@@ -0,0 +1,19 @@
+const calculate_drunk_turtle = (n, day=0) => {
+  day += 1;
+  n += 50;
+  if (n >= 100) {
+    return day;
+  }
+  return calculate_drunk_turtle(n - 30, day);
+}
+
+console.log(`Turtle climbs for ${calculate_drunk_turtle(0)} days.`); // 4
+
+const calculate_handshakes = men => (men - 1) * men / 2;
+console.log(`Men has performed ${calculate_handshakes(10)} handschakes total.`); // 45
+
+const remove_dublicates = (input_words) => {
+  return [...new Set(input_words.split(','))].join(',');
+}
+
+console.log(`'кошка,собака,лошадь,корова,кошка' -> '${remove_dublicates('кошка,собака,лошадь,корова,кошка')}'`);
